0b4a192a7bcebc0e9d6433026f56eade3876755d
ChiLaoTuJiang <3992818597@qq.com>
2024-10-03 21:46:11 +0800

    so
    read_me_too.md md filereader.java

---
+++ filereader.java
@@ -0,0 +1,14 @@
+package com.example 
+
+import java.io.File
+import java.util.JarFile
+import java.util.JarEntry 
+import
+
+public class filereader {
+
+        public void mods {
+        
+        
+      }
+}  
--- libs/read_me_too.md
+++ libs/read_me_too.md
@@ -1,1 +1,2 @@
 Dose anyone hava libraries of forge or fabric？
+If you hava，tell me.
